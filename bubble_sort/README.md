96dad2bb2ab37fd6e69dd038f35a8de99ec0e31b
Daria <daria.92rus@gmail.com>
2020-10-13 22:16:31 +0300

    bubble_sort subproject

---
+++ bubble_sort/README.md
@@ -0,0 +1,10 @@
+# How to build
+* goto project `bubble_sort` folder  
+```cd bubble_sort```  
+* Compile, use gcc  
+```gcc bubleSort.c```  
+* Run   
+```./a.out```  
+
+
+
---
+++ bubble_sort/bublesort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+
+void bubbleSort(int array[],int);
+void printArray(int array[],int);
+
+int main()
+{   
+    int lenght;
+    int *array;
+    
+    printf("Please set array length...\n");
+    scanf("%d",&lenght);
+    printf("Please set array elements...\n");
+    for (int i=0;i<lenght;i++)
+    {
+        scanf("%d",&array[i]);    
+    }
+    //Sort function
+    bubbleSort(array,lenght);
+   
+    //Print sorted array
+    printf("Here's your sorted array:\n");
+    printArray(array,lenght);
+
+    return 0;
+} 
+
+void bubbleSort(int array[],int lenght)
+{
+    int i,j,buf;
+    printf("%i\n",lenght);
+
+    for (i=0;i<lenght;i++)
+    {	
+        for (j=i;j<lenght;j++)
+        {
+            if (array[i]< array[j])
+            {	
+                buf=array[j];
+                array[j]=array[i];
+                array[i]=buf;
+            }
+        }
+    }
+}
+
+void printArray(int array[],int lenght)
+{   
+    for(int i=0;i<lenght;i++)
+    {
+        printf("%d  ",array[i]);
+    }
+    printf("\n");
+
+}
