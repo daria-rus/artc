06cb5015004a27d219006c2fec88f99369081ad4
Daria <daria.92rus@gmail.com>
2020-10-14 01:05:52 +0300

    add enumeration in array

--- print_array/array_print.c
+++ print_array/array_print.c
@@ -2,12 +2,22 @@
 
 int main()
 {
+char *c="dfdfdf";
+int i=0;
+while (*c)
+{
+printf("%c ,", *c);
+c+=1;
+
+}
+printf("\n");
 int array[]={5,7,8,97,77,7,56};
 int n =sizeof(array)/sizeof(int) -1;
-
+int *p = array;
 while (n>=0)
 {
-printf("%d, ",array[n]);
+printf("%d, ",*p);
+p+=1;
 n--;
 }
 
