f1cf32a5d8d760c49c3e93250407461dcdab1f83
Daria <daria.92rus@gmail.com>
2022-12-10 23:10:03 +0300

    update temperature_converter.c: print reverse order

--- celsius_farenheit/temperature_converter.c
+++ celsius_farenheit/temperature_converter.c
@@ -2,8 +2,16 @@
 
 int main (){
 
+//Increase celsius
+
 float c = 5.0/9.0;
  for (int farh=0; farh<=300; farh+=20)
+    printf("%3d %6.1f\n", farh, c*(farh-32));
+
+printf("\nDecreasing celsius:\n");
+//Decrease celsius
+
+ for (int farh=300; farh >= 0; farh-=20)
     printf("%3d %6.1f\n", farh, c*(farh-32));
 
     return 0;
