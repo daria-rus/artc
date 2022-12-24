07d3a3dbc75ef2c1d3e90766401cb5b5f7fdf48b
Daria <daria.92rus@gmail.com>
2022-12-24 23:16:00 +0300

    modify tempersture_converter.c: add symbolic constant, modify test_fl.yml

--- celsius_farenheit/temperature_converter.c
+++ celsius_farenheit/temperature_converter.c
@@ -1,17 +1,20 @@
 # include <stdio.h>
 
+#define LOWER 0  // lower limit tempertature
+#define UPPER 300 //upper limit
+#define STEP 20 // step size
+
 int main (){
 
 //Increase celsius
-
 float c = 5.0/9.0;
- for (int farh=0; farh<=300; farh+=20)
+ for (int farh=LOWER; farh<=UPPER; farh+=STEP)
     printf("%3d %6.1f\n", farh, c*(farh-32));
 
 printf("\nDecreasing celsius:\n");
-//Decrease celsius
 
- for (int farh=300; farh >= 0; farh-=20)
+//Decrease celsius
+ for (int farh=UPPER; farh >= LOWER; farh-=STEP)
     printf("%3d %6.1f\n", farh, c*(farh-32));
 
     return 0;
